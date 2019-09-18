bea617ff0d366ea629e596cd94d8090427d01ef3
Mihail Binev <kodeart@gmail.com>
2019-09-18 09:03:12 +0200

    - removed exception for cloning - added exception for missing method argument

--- DIException.php
+++ DIException.php
@@ -17,22 +17,21 @@ use Psr\Container\{ContainerExceptionInterface, NotFoundExceptionInterface};
 
 class DIException extends KodedException implements ContainerExceptionInterface
 {
-    const E_CIRCULAR_DEPENDENCY    = 1;
-    const E_NON_PUBLIC_METHOD      = 2;
-    const E_CANNOT_INSTANTIATE     = 3;
-    const E_EMPTY_NAME             = 4;
-    const E_INVALID_PARAMETER_NAME = 5;
-    const E_INSTANCE_NOT_FOUND     = 6;
-    const E_CLONING_NOT_ALLOWED    = 7;
+    public const
+        E_CIRCULAR_DEPENDENCY    = 1,
+        E_NON_PUBLIC_METHOD      = 2,
+        E_CANNOT_INSTANTIATE     = 3,
+        E_INVALID_PARAMETER_NAME = 4,
+        E_INSTANCE_NOT_FOUND     = 5,
+        E_MISSING_ARGUMENT       = 6;
 
     protected $messages = [
         self::E_CIRCULAR_DEPENDENCY    => 'Circular dependency detected while creating an instance for ":class"',
         self::E_NON_PUBLIC_METHOD      => 'Failed to create an instance, because the method ":method" is not public',
         self::E_CANNOT_INSTANTIATE     => 'Cannot instantiate the ":type" :class',
-        self::E_EMPTY_NAME             => 'Empty :type name. Provide a valid FQCN',
         self::E_INVALID_PARAMETER_NAME => 'Provide a valid name for the global parameter',
         self::E_INSTANCE_NOT_FOUND     => 'The requested instance :id is not found in the container',
-        self::E_CLONING_NOT_ALLOWED    => 'Cloning the DIContainer is not allowed',
+        self::E_MISSING_ARGUMENT       => 'Required parameter "$:name" is missing at position :position in :function()',
     ];
 
 
@@ -51,19 +50,18 @@ class DIException extends KodedException implements ContainerExceptionInterface
         return new self(self::E_CANNOT_INSTANTIATE, [':class' => $class, ':type' => $type]);
     }
 
-    public static function forEmptyName(string $type): ContainerExceptionInterface
-    {
-        return new self(self::E_EMPTY_NAME, [':type' => $type]);
-    }
-
     public static function forInvalidParameterName(): ContainerExceptionInterface
     {
         return new self(self::E_INVALID_PARAMETER_NAME);
     }
 
-    public static function forCloningNotAllowed(): ContainerExceptionInterface
+    public static function forMissingArgument(string $name, int $position, string $function): ContainerExceptionInterface
     {
-        return new self(self::E_CLONING_NOT_ALLOWED);
+        return new self(self::E_MISSING_ARGUMENT, [
+            ':name' => $name,
+            ':position' => $position,
+            ':function' => $function,
+        ]);
     }
 }
 
