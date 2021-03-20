de84d7c12c1cb58615aa036e1ada1dd45ba1125c
Mihail Binev <kodeart@gmail.com>
2021-03-20 18:08:31 +0100

    - PHP 8 updates - updated exceptions

--- DIReflector.php
+++ DIReflector.php
@@ -23,30 +23,34 @@ use Throwable;
 
 class DIReflector
 {
-    public function newInstance(DIContainerInterface $container, string $class, array $arguments): object
-    {
-        $dependency  = new ReflectionClass($class);
+    public function newInstance(
+        DIContainerInterface $container,
+        string $class,
+        array $arguments
+    ): object {
+        try {
+            $dependency = new ReflectionClass($class);
+        } catch (ReflectionException $e) {
+            throw DIException::forReflectionError($e);
+        }
         $constructor = $dependency->getConstructor();
-
         if ($dependency->isInstantiable()) {
             return $constructor
                 ? new $class(...$this->processMethodArguments($container, $constructor, $arguments))
                 : new $class;
         }
-
-        if (null !== $constructor && false === $constructor->isPublic()) {
-            throw DIException::forNonPublicMethod($constructor->getDeclaringClass()->name . '::' . $constructor->name);
+        if (false === $constructor?->isPublic()) {
+            throw DIException::forNonPublicMethod(
+                $constructor->getDeclaringClass()->name,
+                $constructor->name);
         }
-
-        throw DIException::cannotInstantiate(
-            $dependency->name, $dependency->isInterface() ? 'interface' : 'abstract class'
-        );
+        throw DIException::cannotInstantiate($dependency);
     }
 
     /**
-     * @param DIContainerInterface                  $container
-     * @param ReflectionFunction | ReflectionMethod $method
-     * @param array                                 $arguments
+     * @param DIContainerInterface       $container
+     * @param ReflectionFunctionAbstract $method
+     * @param array                      $arguments
      *
      * @return array
      */
@@ -60,43 +64,38 @@ class DIReflector
         } catch (Throwable $e) {
             $name = $method->getNamespaceName() ?: $method->name;
         }
-
-        $args = array_replace($method->getParameters(), $arguments);
+        $args = \array_replace($method->getParameters(), $arguments);
 
         // PHP quirks...
 
         if ($name === \ArrayObject::class) {
             $args[2] = \ArrayIterator::class;
         }
-
         foreach ($args as $i => $param) {
             if (!$param instanceof ReflectionParameter) {
                 continue;
             }
             $args[$i] = $this->getFromParameterType($container, $param, $arguments);
         }
-
         return $args;
     }
 
     /**
      * @param callable $callable
      *
-     * @return ReflectionMethod | ReflectionFunction
+     * @return ReflectionFunctionAbstract
      * @throws ReflectionException
      */
     public function newMethodFromCallable(callable $callable): ReflectionFunctionAbstract
     {
-        switch (gettype($callable)) {
+        switch (\gettype($callable)) {
             case 'array':
                 return new ReflectionMethod(...$callable);
-
             case 'object';
                 if ($callable instanceof Closure) {
                     return new ReflectionFunction($callable);
                 }
                 return (new ReflectionClass($callable))->getMethod('__invoke');
-
             default:
                 return new ReflectionFunction($callable);
         }
@@ -106,57 +105,57 @@ class DIReflector
         DIContainerInterface $container,
         ReflectionParameter $parameter,
         array $arguments
-    ) {
+    ): mixed {
         if (!$class = $parameter->getType()) {
             return $arguments[$parameter->getPosition()]
                 ?? $this->getFromParameter($container, $parameter);
         }
-
         // Global parameter overriding / singleton instance?
         if ($param = $this->getFromParameter($container, $parameter)) {
             return $param;
         }
-
         if ($parameter->isDefaultValueAvailable()) {
             return $parameter->getDefaultValue();
         }
-
         return $container->new($class->getName());
     }
 
-    protected function getFromParameter(DIContainerInterface $container, ReflectionParameter $parameter)
-    {
+    protected function getFromParameter(
+        DIContainerInterface $container,
+        ReflectionParameter $parameter
+    ): mixed {
         $storage = $container->getStorage();
         $name    = ($parameter->getType() ?? $parameter)->getName();
-
         if (isset($storage[DIContainer::BINDINGS][$name])) {
             $name = $storage[DIContainer::BINDINGS][$name];
         }
-
         if (isset($storage[DIContainer::EXCLUDE][$name])) {
-            if (array_intersect($storage[DIContainer::EXCLUDE][$name], array_keys($storage[DIContainer::SINGLETONS]))) {
+            if (\array_intersect(
+                $storage[DIContainer::EXCLUDE][$name],
+                \array_keys($storage[DIContainer::SINGLETONS])
+            )) {
                 return (clone $container)->new($name);
             }
         }
-
         if (isset($storage[DIContainer::SINGLETONS][$name])) {
             return $storage[DIContainer::SINGLETONS][$name];
         }
-
         if (isset($storage[DIContainer::NAMED]['$' . $parameter->name])) {
             return $storage[DIContainer::NAMED]['$' . $parameter->name];
         }
-
         if ($parameter->isDefaultValueAvailable()) {
             return $parameter->getDefaultValue();
         }
 
-        $type = $parameter->getType();
-        if ($type && $type->isBuiltin()) {
-            throw DIException::forMissingArgument($name, $parameter->getPosition(),
-                $parameter->getDeclaringClass()->name . '::' . $parameter->getDeclaringFunction()->name);
+        // [EXPERIMENTAL] in case of functions without arguments
+        // (ie. PHP built-in var_dump, print_r, etc)
+        if (null === $parameter->getDeclaringClass()) {
+            throw DIException::forUnprocessableFunctionParameter($parameter, \debug_backtrace());
         }
 
+        if ($parameter->getType()?->isBuiltin()) {
+            throw DIException::forMissingArgument($name, $parameter);
+        }
         return null;
     }
 }
