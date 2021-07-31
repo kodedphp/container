aeefd633d70188315b454d53cb45de4d830e1823
Mihail Binev <kodeart@gmail.com>
2021-07-31 10:42:09 +0200

    - test for deferred binding (with late mapping)

--- Tests/Unit/BindMethodTest.php
+++ Tests/Unit/BindMethodTest.php
@@ -24,6 +24,24 @@ class BindMethodTest extends DITestCase
         $this->assertTrue($this->di->has(TestClassWithInterfaceAndNoConstructor::class));
     }
 
+    public function testInterfaceToInterfaceBinding()
+    {
+        $this->di->bind(TestChildInterface::class);
+        $this->di->bind(TestInterface::class, TestClassWithInterfaceAndNoConstructor::class);
+
+        $instance1 = $this->di->new(TestDependencyWithExtendedInterface::class);
+        $instance2 = $this->di->new(TestChildInterface::class);
+
+        $this->assertInstanceOf(TestDependencyWithExtendedInterface::class, $instance1);
+        $this->assertInstanceOf(TestClassWithInterfaceAndNoConstructor::class, $instance1->getDependency());
+        $this->assertInstanceOf(TestClassWithInterfaceAndNoConstructor::class, $instance2);
+
+        $this->assertNotSame(
+            $instance1->getDependency(),
+            $instance2,
+            'The container creates 2 separate instances');
+    }
+
     protected function createContainer(): DIContainer
     {
         return new DIContainer;
